1d92ad73a7ea896c299af59901167f608baa63b2
Luna <williamb19084@gmail.com>
2025-12-12 21:14:46 -0500

    Added functions to manipulate player records

---
+++ constants.py
@@ -0,0 +1,4 @@
+# This file stores all constants for the program
+from typing import Final
+
+EXAMPLE: Final = 5;
---
+++ playerfiles/createPlayer.py
@@ -0,0 +1,74 @@
+# A file that defines the functions to manage the player json file
+import json
+
+# Adds a player to the Player.json file. The player can not already be in the file
+def addPlayerRecord(name, player_class, country): # {
+  
+    # Checks if player is in the file
+    if checkPlayerRecord(name):
+        raise LookupError
+    
+    players_json = readPlayerFile()
+
+    data = [player_class, country]
+    players_json[name] = data
+    
+    writePlayerFile(players_json)
+    
+    return
+# }
+
+# Deletes a users from Players.json file. The player must be in the file
+def deletePlayerRecord(name): # {
+    
+    # Checks if player is in the file
+    if (not checkPlayerRecord(name)):
+        raise LookupError
+    
+    players_json = readPlayerFile() 
+
+    players_json.pop(name)
+    
+    writePlayerFile(player_json)
+
+    return
+# }
+
+# Reads the Player.json file and returns dictionary
+def readPlayerFile(): # {
+    
+    try: 
+        with open("Players.json", "r") as file: 
+            players_json = json.load(file)
+    except:
+        players_json = {}
+
+    return players_json
+# }
+
+# Writes to the Player.json file. Takes a dictionary
+def writePlayerFile(dictionary): # {
+
+    with open("Players.json", "w") as file: 
+        file_contents = json.dumps(dictionary, indent=4)
+        file.write(file_contents)
+    
+    return
+# }
+
+# Checks if a player is in the Player.json file
+def checkPlayerRecord(name): # {
+    
+    players_json = readPlayerFile()
+ 
+    if name in players_json:
+        return True
+    else:
+        return False
+# }
+
+addPlayerRecord("D", "Rogue", "1")
+addPlayerRecord("B", "Barb", "2")
+addPlayerRecord("A", "Cleric", 3)
+deletePlayerRecord("Ai")
+
